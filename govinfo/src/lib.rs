be701992db9d20688ea8182b104830463824e52b
blackerby <wmblackerby@gmail.com>
2024-03-16 10:38:05 -0400

    Initial type sketch

--- govinfo/src/lib.rs
+++ govinfo/src/lib.rs
@@ -1,14 +1,41 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[allow(unused)]
+use serde::Deserialize;
+use serde::Serialize;
+
+#[allow(dead_code)]
+#[derive(Serialize, Deserialize)]
+struct Collection {
+    collection_code: String,
+    collection_name: String,
+    package_count: usize,
+    granule_count: Option<usize>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[allow(dead_code)]
+#[derive(Serialize, Deserialize)]
+struct Package {
+    package_id: String,
+    last_modified: String,
+    package_link: String,
+    doc_class: String,
+    title: String,
+    congress: String,
+    date_issued: String,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[allow(dead_code)]
+#[derive(Serialize, Deserialize)]
+struct Granule {
+    title: String,
+    granule_id: String,
+    granule_class: String,
+    md5: String,
+}
+
+#[allow(dead_code)]
+#[derive(Serialize, Deserialize)]
+struct Relationship {
+    relationship_link: String,
+    collection: String,
+    relationship: String,
 }
